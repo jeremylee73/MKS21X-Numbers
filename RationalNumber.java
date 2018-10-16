6f0e8b4276dee9db9c8afb768c2d5d4b5b3b3f76
Jeremy Lee <jlee00@stuy.edu>
2018-10-16 00:22:51 -0400

    Added headers for rational and real

--- RationalNumber.java
+++ RationalNumber.java
@@ -1,3 +1,24 @@
-public class RationalNumber {
-  
+public class RationalNumber extends RealNumber{
+
+  private int numerator;
+  private int denominator;
+
+  public RationalNumber(int num, int den){
+    super(1.0);
+    numerator = num;
+    denominator = den;
+  }
+
+  public int getValue(){
+    return 0;
+  }
+
+  public int compareQ(RationalNumber q){
+    return 0;
+  }
+
+  public String toString(){
+    return "";
+  }
+
 }
--- RealNumber.java
+++ RealNumber.java
@@ -1,3 +1,21 @@
-public class RealNumber {
-  
+public class RealNumber{
+
+  private double number;
+
+  public RealNumber(double num){
+    number = num;
+  }
+
+  public double getReal(){
+    return 0.0;
+  }
+
+  public String toString(){
+    return "";
+  }
+
+  public int compareR(RealNumber r){
+    return 0;
+  }
+
 }
